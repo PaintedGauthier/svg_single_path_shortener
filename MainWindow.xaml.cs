0a574f72f4d7d27f0c076e3450ee7add0b5de9c1
jgauthier <jgauthier@aagcnc.com>
2021-11-02 16:29:56 -0400

    cleanup , fixed warnings / messages

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,18 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace svg_shortener
 {
@@ -26,58 +15,66 @@ namespace svg_shortener
             InitializeComponent();
             Loaded += (m, n) =>
             {
-                tbRawData.TextChanged += tbRawData_TextChanged;
+                tbRawData.TextChanged += TbRawData_TextChanged;
             };
         }
 
-        public string fix_num(string num)
+        public string Fix_num(string num)
         {
             if (!double.TryParse(num, out double result))
+            {
                 return "0";
+            }
 
-            var res = result.ToString("0.###");
+            string res = result.ToString("0.###");
             if (res == "0.0")
+            {
                 return "0";
+            }
             else if (res.StartsWith("0."))
+            {
                 res = res.Replace("0.", ".");
+            }
             else if (res.StartsWith("-0."))
+            {
                 res = res.Replace("-0.", "-.");
+            }
+
             return res;
         }
 
-        private void tbRawData_TextChanged(object sender, TextChangedEventArgs e)
+        private void TbRawData_TextChanged(object sender, TextChangedEventArgs e)
         {
-            var text = (sender as TextBox).Text;
+            string text = (sender as TextBox).Text;
             StringBuilder sb = new StringBuilder();
-            var parts = text.Split(" ");
-            int comma;
+            string[] parts = text.Split(" ");
             string[] list;
             Regex only_letters = new Regex(@"^[a-zA-Z]+$");
-            foreach (var part in parts)
+            foreach (string part in parts)
             {
                 if (only_letters.IsMatch(part))
                 {
-                    sb.Append(part);
+                    _ = sb.Append(part);
                 }
                 else
                 {
                     list = part.Split(",");
                     if (list.Length == 2)
                     {
-                        sb.Append(fix_num(list[0]));
-                        sb.Append(",");
-                        sb.Append(fix_num(list[1]));
+                        _ = sb.Append(Fix_num(list[0]));
+                        _ = sb.Append(",");
+                        _ = sb.Append(Fix_num(list[1]));
                     }
                     else if (list.Length == 1)
                     {
-                        sb.Append(fix_num(list[0]));
+                        _ = sb.Append(Fix_num(list[0]));
                     }
                     else
                     {
 
                     }
                 }
-                sb.Append(" ");
+                _ = sb.Append(" ");
             }
             tbFixedData.Text = sb.ToString().Trim();
             tbTitle1.Text = "Raw svg data (" + text.Length + ")";
